910fd6301de8c9be555ab735d5c5416109ba9048
MaxTTG <m-tarazanov@list.ru>
2024-10-27 22:51:26 +0300

    HW5 fix

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 // #include "HW2/hw2.hpp"
 // #include "HW1/hw1.hpp"
 // #include "HW3/hw3.hpp"
-#include "HW4/hw4.hpp"
+// #include "HW4/hw4.hpp"
+#include "HW5/hw5.hpp"
 
 int main() {
     // hw1::solution();
     // hw2::solution();
     // hw3::solution();
-    hw4::solution();
+    // hw4::solution();
+    hw5::solution();
 }
